63964b017711f9974e71d820603fd2a1365d5391
Castillo <andresinho826@gmail.com>
2025-08-15 20:24:12 -0500

    funciones

---
+++ funciones.py
@@ -0,0 +1,36 @@
+
+def sumar(num1,num2):
+    resultado = num1 + num2
+    print("suma: ", resultado)
+    dividir(2,2)
+
+def restar(num1,num2):
+    resultado = num1 - num2
+    print("resta: ", resultado)
+
+def dividir(num1,num2):
+    resultado = num1 / num2
+    print("division: ", resultado)
+    restar(2,2)
+
+def multiplicar(num1,num2):
+    resultado = num1 * num2
+    print("multiplicacion", resultado)
+    sumar(2,2)
+
+
+
+#----------------------------------------------------------- ejemplo
+
+
+
+def saludar(nombre):
+    print("Hola ", nombre , "Bienvenido a las clases de python!!")
+
+
+saludar("yoandri")
+saludar("kathe")
+saludar("tati")
+    
+
+    
